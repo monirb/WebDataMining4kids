f284e4f6d92ca99b9fe56618c65014b671861057
monirb <creativesolving@gmail.com>
2017-04-12 22:54:48 +0100

    Add files via upload

---
+++ front end/learning.php
@@ -0,0 +1,104 @@
+<!--
+Author: Dr. Moniruzzaman Bhuiyan 
+Author URL: http://mb.banglaworld.org
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>MI-CHILDREN: Intelligence for special needs: Visual Learning, Activity Management, Behaviour Control and Social Interaction Development</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- start plugins -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!----font-Awesome----->
+   	<link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+<!----font-Awesome----->
+</head>
+<body>
+<div class="header_bg1">
+<div class="container">
+	<div class="row header">
+		<div class="logo navbar-left">
+			<h1><a href="index.html">MI-CHILDREN: Intelligence for special </a></h1>
+            
+		</div>
+		<div class="h_search navbar-right">
+			<form>
+				<input type="text" class="text" value="Enter text here" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter text here';}">
+				<input type="submit" value="search">
+			</form>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="row h_menu">
+		<nav class="navbar navbar-default navbar-left" role="navigation">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.html">Home</a></li>
+		        <li class="active"><a href="learning.php">Learning</a></li>
+		        <li><a href="social.html">Social Interaction</a></li>
+		        <li><a href="behaviour.html">Behaviour</a></li>
+                <li><a href="activity.html">Activity</a></li>
+		        <li><a href="contact.html">Contact</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		    <!-- start soc_icons -->
+		</nav>
+		<div class="soc_icons navbar-right">
+			<ul class="list-unstyled text-center">
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+			</ul>	
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+</div>
+<div class="main_btm"><!-- start main_btm -->
+	<div class="container">
+		<div class="main row">
+			<div class="col-md-6 content_left">
+				<img src="images/det_pic.jpg" alt="" width="800" height="300" class="img-responsive"/>
+			</div>
+		  <div class="col-md-6 content_right"><h1>Visual Learing	</h1>
+			  <h2 class="para"><strong>Takes plain text (for example, a paragraph of a story) as an input to generate multimodal learning materials from the Internet sources could be of great help to both teachers and children with special needs.</strong></h2>
+		  </div>
+            <p class="para">The solution which at first extracts the keywords from the plain text provided using word co-occurrences method. The co-occurrence distribution between frequent terms and other terms in a sentence are used to define the relative importance of a term. Thus extracted keywords are used to search for the multimodal elements (texts, images and video clips) and other similar stories through search engine APIs. After the search operation, URL filtering, domain filtering and web content analysis based filtering methods are used to remove unwanted materials. </p>
+		<div class="col-md-3 images_1_of_4 bg1 text-center">
+			<a href="http://mbdotnet.somee.com/" class="fa-btn btn-1 btn-1e">Get visual learning from story</a></div>
+		</div>
+<div class="footer_bg"><!-- start footer -->
+	<div class="container">
+		<div class="row  footer">
+			<div class="copy text-center">
+				<p class="link"><span>&#169; All rights reserved MI-Children.org</span></p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ front end/story-from.php
@@ -0,0 +1,159 @@
+<?php require_once('Connections/mic.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO intelligences (id, scenariotype, scenariodata) VALUES (%s, %s, %s)",
+                       GetSQLValueString($_POST['id'], "int"),
+                       GetSQLValueString($_POST['scenariotype'], "text"),
+                       GetSQLValueString($_POST['scenariodata'], "text"));
+
+  mysql_select_db($database_mic, $mic);
+  $Result1 = mysql_query($insertSQL, $mic) or die(mysql_error());
+}
+?>
+<!--
+Author: Monir Bhuiyan 
+Author URL: 
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>MI-CHILDREN: Intelligence for special needs: Visual Learning, Activity Management, Behaviour Control and Social Interaction Development</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- start plugins -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!----font-Awesome----->
+   	<link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+<!----font-Awesome----->
+</head>
+<body>
+<div class="header_bg1">
+<div class="container">
+	<div class="row header">
+		<div class="logo navbar-left">
+			<h1><a href="index.html">MI-CHILDREN: Intelligence for special </a></h1>
+            
+		</div>
+		<div class="h_search navbar-right">
+			<form>
+				<input type="text" class="text" value="Enter text here" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter text here';}">
+				<input type="submit" value="search">
+			</form>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="row h_menu">
+		<nav class="navbar navbar-default navbar-left" role="navigation">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.html">Home</a></li>
+		        <li class="active"><a href="learning.php">Learning</a></li>
+		        <li><a href="social.html">Social Interaction</a></li>
+		        <li><a href="behaviour.html">Behaviour</a></li>
+                <li><a href="activity.html">Activity</a></li>
+		        <li><a href="contact.html">Contact</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		    <!-- start soc_icons -->
+		</nav>
+		<div class="soc_icons navbar-right">
+			<ul class="list-unstyled text-center">
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+			</ul>	
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+</div>
+<div class="main_btm"><!-- start main_btm -->
+	<div class="container">
+		    <h1>Paste or write a story here</h1>
+            <form method="post" name="form1" action="<?php echo $editFormAction; ?>">
+              <table align="left">
+                <tr valign="baseline">
+                  <td><textarea name="scenariodata" cols="200" rows="5"></textarea></td>
+                </tr>
+                <tr valign="baseline">
+                
+                  <td align="center"><input class="fa-btn btn-1 btn-2e"  type="submit" value="Submit"></td>
+                </tr>
+              </table>
+              <input type="hidden" name="id" value="">
+              <input type="hidden" name="scenariotype" value="Learning">
+              <input type="hidden" name="MM_insert" value="form1">
+            </form>
+   
+            
+		</div>
+<div class="footer_bg"><!-- start footer -->
+	<div class="container">
+		<div class="row  footer">
+			<div class="copy text-center">
+				<p class="link"><span>&#169; All rights reserved MI-Children.org</span></p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
